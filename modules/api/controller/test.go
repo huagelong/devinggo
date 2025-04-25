a9d6146518b6ac6af9a90b3945e493cacfb5cb41
kaihui.wang <hpuwang@gmail.com>
2025-04-25 13:37:20 +0800

    整理

--- modules/api/controller/test.go
+++ modules/api/controller/test.go
@@ -25,11 +25,6 @@ type testController struct {
 
 func (c *testController) Index(ctx context.Context, in *api.IndexReq) (out *api.IndexRes, err error) {
 	out = &api.IndexRes{}
-	user, _ := service.SystemUser().GetInfoById(ctx, 1)
-	g.Log().Debug(ctx, user)
-	cron.GetWorkerLists()
-	ls := cron.GetWorkerLists()
-	g.Log().Debug(ctx, ls)
 	out.Data = "Hello, World!"
 	return
 }
